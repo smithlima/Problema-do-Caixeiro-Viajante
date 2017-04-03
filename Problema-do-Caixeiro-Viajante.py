02bb02cf0051326d105a4481a1e54783dd86a38b
smithlima <smith.lima@eng.ci.ufpb.br>
2017-04-03 04:17:03 +0000

    Add files via upload

---
+++ Problema-do-Caixeiro-Viajante.py
@@ -0,0 +1,66 @@
+import sys
+import copy
+
+
+n = int(input())
+matrix = []
+for j in range(n):
+	matrix.append([int(i) for i in input().split()])
+
+g = {}
+p = []
+
+def main():
+    for x in range(1, n):
+        g[x + 1, ()] = matrix[x][0]
+    
+    get_minimum(1,range(2,n+1))
+    
+    solution = p.pop()
+    a = [1]
+    a.append(solution[1][0])
+
+    for x in range(n - 2):
+        for new_solution in p:
+            if tuple(solution[1]) == new_solution[0]:
+                solution = new_solution
+                a.append(solution[1][0])
+                break
+    a.append(1)
+   
+    b = a.copy()
+    b.remove(b[0])
+    b.append(a[0])
+    cost = 0
+    for i,j in zip(a,b):
+        if i != j:
+            cost+=matrix[i-1][j-1]
+    print("Valor: ",cost)
+    print("Circuito: ", a)
+    
+    return
+
+def get_minimum(k, a):
+    if (k, a) in g:
+        # Already calculated Set g[%d, (%s)]=%d' % (k, str(a), g[k, a]))
+        return g[k, a]
+
+    values = []
+    all_min = []
+    for j in a:
+        set_a = copy.deepcopy(list(a))
+        set_a.remove(j)
+        all_min.append([j, tuple(set_a)])
+        result = get_minimum(j, tuple(set_a))
+        values.append(matrix[k-1][j-1] + result)
+
+    # get minimun value from set as optimal solution for
+    g[k, a] = min(values)
+    p.append(((k, a), all_min[values.index(g[k, a])]))
+
+    return g[k, a]
+
+
+if __name__ == '__main__':
+    main()
+sys.exit(0) 
